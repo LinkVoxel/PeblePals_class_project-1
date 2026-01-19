e70f15b9ffdd368a11c47a3cc042187c73258798
LinkVoxel <swiftpulse96@gmail.com>
2026-01-19 14:57:04 +0000

    testimonial form start

--- scripts/testimonials.js
+++ scripts/testimonials.js
@@ -40,31 +40,26 @@ for (let review of testimonialList) {
 
     let stars = '';
     for(let i = 0; i < 5; i++){
-        if(i < review.rating){
+        if(i < review.rating){  
                 stars += `<img class="star-img" src="/assets/star_yellow.svg">`
         }else {
-            stars  += `<img class ='star-img' src = '/assets/star_black.svg>`
+            stars  += `<img class ='star-img' src ="/assets/star_black.svg">`
         }
     }
 //tesdsdffsdfdds
 
     let testimonialToAdd = `
-    <div class="testimonials-box">
-
-                <div class="testimonial">
-                    <div class="t-rating-row">
-                        <div class="t-stars">
-                            ${stars}
-                            <img class="star-img" src="/assets/star_yellow.svg">
-                            <img class="star-img" src="/assets/star_yellow.svg">
-                            <img class="star-img" src="/assets/star_yellow.svg">
-                        </div>
-                        <p class="t-name">~${review.name}</p>
-                    </div>
-                    <p class="t-message">
-                        ${review.name}
-                    </p>
+        <div class="testimonial">
+            <div class="t-rating-row">
+                <div class="t-stars">
+                    ${stars}
                 </div>
+                <p class="t-name">~${review.name}</p>
+            </div>
+            <p class="t-message">
+                ${review.review}
+            </p>
+        </div>
                 `
 
                 testimonialBox.innerHTML += testimonialToAdd
